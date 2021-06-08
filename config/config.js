e8538cd40ddf8f9a1ebeeb1f2f1ef8e23cfaf6c8
sweetsawant2 <sweetsawant2@gmail.com>
2021-06-08 19:16:27 +0530

    server changes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    uri: "mongodb://127.0.0.1:27017",
+    // uri: "mongodb://127.0.0.1:27017",
+    uri :"mongodb+srv://medical:t6nHbWYGmHCrZ8xK@cluster0.zpebv.mongodb.net/Cluster0?retryWrites=true&w=majority",
     dbName: "medical",
     secret: "supersecretformedical"
 }
+
