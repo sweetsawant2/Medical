f96e42455e21aabeb78f64e5a94fff9de09386a2
shubham sawant <40673678+sweetsawant2@users.noreply.github.com>
2021-07-11 21:07:16 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // uri: "mongodb://127.0.0.1:27017",
-    uri :"mongodb+srv://arjun:5VoJIC5V3ytdZ2yz@cluster0.twurf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    uri :"mongodb+srv://arjun:zwIPubNE00gyOlwy@medical.twurf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     dbName: "medical",
     secret: "supersecretformedical"
 }
